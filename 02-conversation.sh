5d0d4c24f8179f269adf8c76bdfc4705fb825564
yash8575 <allamyash0609@gmail.com>
2026-03-10 10:04:48 +0100

    conv

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Suresh::Hello ramesh,how are you"
+echo "Ramesh::Hi suresh, i am fine thnaks for asking.How are you doing"
+echo "suresh ::doing fine. what are you learning Ramesh?"
+echo "Ramesh : i am learning shell.what about you?"
