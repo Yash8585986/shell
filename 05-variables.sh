fc57edcc75efdda7064fe24fcd4e73bf0f143816
yash8575 <allamyash0609@gmail.com>
2026-06-02 14:57:42 +0200

    roboshop

--- 05-variables.sh
+++ 05-variables.sh
@@ -4,3 +4,6 @@ echo "Please enter the username::"
 read USER_NAME
 echo "print the user name ${USER_NAME}"
 
+echo "Please enter the password::"
+read -s PASSWORD
+echo "Print the password ${PASSWORD}"
