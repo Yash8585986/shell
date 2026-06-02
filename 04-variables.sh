f14cac54c765fc61d48e586aedcb9266373ceb53
yash8575 <allamyash0609@gmail.com>
2026-06-02 14:45:45 +0200

    roboshop

--- 04-variables.sh
+++ 04-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-PERSON1="Trump"
-PERSON2="Putim"
+PERSON1="$1"
+PERSON2="$2"
 
 echo "${PERSON1}::Hello ${PERSON2}, how are you?"
 echo "${PERSON2}::Hi ${PERSON1}, I am fine thanks for asking. How are you doing?"
