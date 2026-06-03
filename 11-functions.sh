97e202ed55738b8a67840d99b4079df4ae20ff35
yash8575 <allamyash0609@gmail.com>
2026-06-03 14:35:58 +0200

    roboshop

---
+++ 11-functions.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "Please run the script with root user"
+    exit 1
+fi
+
+VALIDATE() {
+
+    if [ $1 -ne 0 ]; then
+
+    echo "installation of $2 failed"
+    exit 1
+else
+    echo "Installation of $2 successful"
+}
+
+
+
+dnf install nginx -y
+VALIDATE $? "nginx"
+
+dnf install nodejs -y
+VALIDATE $? "nodejs"
+
+dnf install mysql -y
+VALIDATE $? "mysql"
+
