6f80c9d339d28c5f2238be17e4c5adb5dd26cd2c
yash8575 <allamyash0609@gmail.com>
2026-06-03 16:48:09 +0200

    roboshop

---
+++ 14-loops.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-scripts"
+LOG_FILE="/var/log/shell-scripts/$0.log"
+
+if [ $USERID -ne 0 ]; then
+    echo "Please run the script with root user" | tee -a $LOG_FILE
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+
+    if [ $1 -ne 0 ]; then
+
+    echo "installation of $2 failed" | tee -a $LOG_FILE
+    exit 1
+else
+    echo "Installation of $2 successful" | tee -a $LOG_FILE
+    fi
+}
+
+
+for i in $@
+do
+dnf install $i -y &>> $LOG_FILE
+VALIDATE $? "Installion of $i"
+done
+
+# dnf install nginx -y &>> $LOG_FILE
+# VALIDATE $? "nginx"
+
+# dnf install nodejs -y &>> $LOG_FILE
+# VALIDATE $? "nodejs"
+
+# dnf install mysql -y &>> $LOG_FILE
+# VALIDATE $? "mysql"
+
