f251a85cf8ce2755a973a89d201d0f8e53227917
yash8575 <allamyash0609@gmail.com>
2026-06-03 17:43:31 +0200

    roboshop

--- 16-colors.sh
+++ 16-colors.sh
@@ -5,4 +5,4 @@ GREEN="\e[32m"
 YELLOW="\e[33m"
 RESET="\e[0m"
 
-echo -e "$RED This is red color $RESET , i am learning shell scripting"
+echo -e "$RED This is red color $RESET , $YELLOW i am learning shell scripting $RESET"
