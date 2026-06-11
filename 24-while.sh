9959b6be8fb327341a0fd89a8ec76e256f551368
yash8575 <allamyash0609@gmail.com>
2026-06-11 10:26:13 +0200

    shell

--- 24-while.sh
+++ 24-while.sh
@@ -2,13 +2,25 @@
 
 #!/bin/bash
 
-counter=1
+# counter=1
 
-while [ $counter -le 5 ]
-do
-    echo "Loop iteration: $counter"
-    # Increment the counter by 1
-    ((counter++))
-done
+# while [ $counter -le 5 ]
+# do
+#     echo "Loop iteration: $counter"
+#     # Increment the counter by 1
+#     ((counter++))
+# done
 
-echo "Loop finished!"
+# echo "Loop finished!"
+
+#!/bin/bash
+
+# Path to the file you want to read
+# FILE_PATH="servers.txt"
+
+# Loop through each line of the file
+while IFS= read -r line
+do 
+echo "$line"
+
+done < 21-script-1.sh
